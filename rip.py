1e76480758fae3b841c02be6230c412fde5ca1e0
Giovanna <giovanna.blasco@hotmail.com>
2017-09-20 17:56:08 -0300

    quase sucesso

--- rip.py
+++ rip.py
@@ -6,6 +6,13 @@ import socket
 import os
 import time
 import json
+from threading import Thread
+from threading import Lock
+
+#globais
+timer = random.randrange(8,10)
+lock = Lock()
+
 
 class Node():
     def __init__(self, ID):
@@ -13,6 +20,7 @@ class Node():
         self.neighbour = list() # lista de vizinhos
         self.vector = list() # vetor de custos
         self.nodesqt = 0 # quantidade de nós
+        self.initVector()
 
     def sendVector(self):
         global timer
@@ -22,38 +30,37 @@ class Node():
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 server_address = ('localhost', 52000 + self.neighbour[n])
                 sock.connect(server_address)
-                msg = "[" + str(self.ID) + "," + json.dumps(vector) + "]"
+                msg = "[" + str(self.ID) + "," + json.dumps(self.vector) + "]"
                 sock.send(msg.encode())
-                timer = random.randrange(4, 9)
-                print ("Enviei minha tabela para: " + self.neighbour[n])
+                print ("Enviei minha tabela para: " + str(self.neighbour[n]))
             except Exception as e:
                 print(e)
 
-    def updateVector(self, nbvector):
+    def updateVector(self, nbvector):  #[id, [] ]
         # flag de controle de atualização
-        updated = 0
+        updated = False
 
         # precisa receber ID do vizinho
-        print("Recebi uma atualização do nó " + nbID)
-        print("Vetor atual do nó " + self.ID)
+        print("Recebi uma atualização do nó " + str(nbvector[0]))
+        print("Meu vetor de distâncias atual:")
         self.printVector()
 
         # recebe a distancia ate o vizinho
-        distnode = nbvector[self.ID]
+        distnode = nbvector[1][self.ID]
 
-        for n in range(0, len(nbvector)):
+        for n in range(0, len(self.vector)):
             # se a (distancia ate o vizinho + a distancia do vizinho ate o nó n) for menor
             # que a distancia guardada na tabela, substitue. se não, mantém.
-            if (distnode + nbvector[n]) < self.vector[n]:
-                self.vector[n] = distnode + nbvector[n]
-                updated++
+            if (distnode + nbvector[1][n]) < self.vector[n]:
+                self.vector[n] = distnode + nbvector[1][n]
+                updated = True
 
-        if updated > 0:
-            print ("Vetor de " + self.ID + " atualizado")
+        if updated == True:
+            print ("Vetor de " + str(self.ID) + " atualizado")
             self.printVector()
             # aqui chama o envia tabela? ou manda mesmo quando não tiver atualizada?
         else:
-            print ("Vetor de " + self.ID + " não teve atualização")
+            print ("Vetor de " + str(self.ID) + " não teve atualização")
 
         # Initialization:
             # for all destinations y in N:
@@ -75,17 +82,87 @@ class Node():
         with open('topology.json') as data_file:
             data = json.load(data_file)
         self.vector = data[self.ID]
-    	print ("Tabela do nó " + self.ID + " inicializada")
+        self.nodesqt = len(data)
+        print ("Tabela do nó " + str(self.ID) + " inicializada")
         # preenche lista de vizinhos (não precisa ser atualizada nunca)
         for n in range(0, len(self.vector)):
-            if self.vector[n] != 999:
+            if self.vector[n] != 999 and n != self.ID:
                 self.neighbour.append(n)
         # dispara timer para enviar o update
 
     def printVector(self):
-        print "Distâncias:\n"
+        print ("Distâncias:\n")
         for n in range(0, len(self.vector)):
-            print("Nó " + n + ": " + self.vector[n])
+            print("Nó " + str(n) + ": " + str(self.vector[n]))
 
-class Listener():
+
+class TicTacker(Thread): # decrementa o timer, e ao zerar, chama o enviar mensagem
     def __init__(self):
+        Thread.__init__(self)
+
+    def run(self):
+        global timer
+        global lock
+        while 1:
+            with lock:
+                if(timer > 0):
+                    timer = timer - 1
+                else:
+                    n.sendVector()
+                    timer = random.randrange(4, 9)
+            time.sleep(1)
+
+class Listener(Thread):
+    def __init__(self, ID, nodesqt):
+        self.port = 52000 + int(ID)
+        self.nodesqt = int(nodesqt)
+        Thread.__init__(self)
+
+    def run(self):
+        global n
+        global timer
+        global lock
+
+        serverPort = self.port
+        serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        try:
+            serverSocket.bind(('',serverPort))
+            serverSocket.listen(1)
+            print ('*** Subindo o nó de número ', self.port - 52000,' no total de ', self.nodesqt,'***')
+            print('*** No ar através da porta: ', self.port,' ***' )
+
+            while 1:
+                connectionSocket, addr = serverSocket.accept()
+                try:
+                    msg = connectionSocket.recv(512) #[id, [vector]]
+                    msg = msg.decode('utf-8')
+                    print ("Recebi da rede: ", msg)
+                    data = json.loads(str(msg))
+                    n.updateVector(data)
+                    with lock:
+                        timer = random.randrange(4,9)
+
+                except Exception as e :
+                    exec_type, exec_obj, exec_tb = sys.exc_info()
+                    print ("Erro:", exec_type, exec_tb.tb_lineno,"\n",e)
+                    sys.exit(2)
+
+        except Exception as e :
+                print (e)
+
+def main():
+    if( len(sys.argv)!=2):
+        print("Chamada inválida use: $ python3 rip.py NUM_NO")
+        sys.exit(1)
+    global n
+
+    n = Node(int(sys.argv[1]))
+    nodesqt = n.nodesqt
+    listener = Listener(int(sys.argv[1]), nodesqt)
+    t = TicTacker()
+
+    listener.start()
+    t.start()
+
+if __name__ == "__main__":
+    main()
